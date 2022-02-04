f593bc94885cf975635e51bd824f8c8bb9187e1c
ar-personal <reid0alex@gmail.com>
2022-02-04 23:18:31 +0000

    2015.5 part two

--- input files/2015.5.py
+++ input files/2015.5.py
@@ -6,9 +6,35 @@ def main():
     with open("2015.5.txt") as file:
         for line in file:
             line = line.strip("\n")
-            if contains3Vowels(line) and contains2CharInRow(line) and not containsBLString(line, blacklist):
+            # if contains3Vowels(line) and contains2CharInRow(line) and not containsBLString(line, blacklist):
+            if containsLetterPairs(line) and charRepeatsWithGap(line, 1):
                 niceCount += 1
+
     print(niceCount)
+
+
+def containsLetterPairs(string):
+    substr = ''
+    i = 0
+    while i < len(string) -1:
+        substr = string[i:i + 2]
+        if string.count(substr) >= 2:
+            return True
+        i += 1
+    return False
+
+
+def charRepeatsWithGap(string, gap):
+    i = 0
+    while i < len(string):
+        if i + (gap + 1) >= len(string):
+            return False
+        char = string[i]
+        nextChar = string[i + (gap + 1)]
+        if nextChar == char:
+            return True
+        i += 1
+    return False
 
 
 def contains3Vowels(string):
